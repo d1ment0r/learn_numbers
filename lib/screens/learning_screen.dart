815f99ab79bcedc53994a7ae5f49485d5904fdf9
Dmitrii Smirnov <sd@isu-it.ru>
2022-11-24 22:13:29 +0300

    all ok

--- lib/screens/learning_screen.dart
+++ lib/screens/learning_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
+import 'package:flutter/services.dart';
+import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:learn_numbers/models/globals.dart' as globals;
 import 'package:learn_numbers/models/language.dart';
@@ -16,32 +20,14 @@ class LearningScreen extends StatefulWidget {
 
 class _LearningScreenState extends State<LearningScreen> {
   int _currentStep = 0;
-  final List<int> _arrayNumbers = [
-    0,
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    20,
-    30,
-    40,
-    50,
-    60,
-    70,
-    80,
-    90,
-    100
-  ];
+  bool _isElevated = false;
+  List<int> arrayNumbers = [];
   bool _speechButtonOn = false;
+  final searchController = TextEditingController();
 
   @override
   void initState() {
+    fillArrayNumbers();
     super.initState();
   }
 
@@ -54,59 +40,159 @@ class _LearningScreenState extends State<LearningScreen> {
         });
       });
     }
+    if (_isElevated) {
+      Future.delayed(const Duration(milliseconds: 150), () {
+        developer.log('press +0 button');
+        setState(() {
+          _isElevated = false;
+        });
+      });
+      //
+    }
 
-    return SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Column(
+      children: [
+        topRowWidget(),
+        Expanded(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: ListView.builder(
+              itemCount: arrayNumbers.length,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    rowNumbersWidget(index),
+                    dividerWidget(),
+                  ],
+                );
+              }),
+        ),
+      ],
+    );
+  }
+
+  Container topRowWidget() {
+    return Container(
+      margin: const EdgeInsets.only(
+          top: 15.0, left: 16.0, right: 16.0, bottom: 8.0),
+      child: Row(
         children: [
-          Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const SizedBox(
-                height: 10,
-              ),
-              rowNumbersWidget(0),
-              dividerWidget(),
-              rowNumbersWidget(1),
-              dividerWidget(),
-              rowNumbersWidget(2),
-              dividerWidget(),
-              rowNumbersWidget(3),
-              dividerWidget(),
-              rowNumbersWidget(4),
-              dividerWidget(),
-              rowNumbersWidget(5),
-              dividerWidget(),
-              rowNumbersWidget(6),
-              dividerWidget(),
-              rowNumbersWidget(7),
-              dividerWidget(),
-              rowNumbersWidget(8),
-              dividerWidget(),
-              rowNumbersWidget(9),
-              dividerWidget(),
-              rowNumbersWidget(10),
-              dividerWidget(),
-              rowNumbersWidget(11),
-              dividerWidget(),
-              rowNumbersWidget(12),
-              dividerWidget(),
-              rowNumbersWidget(13),
-              dividerWidget(),
-              rowNumbersWidget(14),
-              dividerWidget(),
-              rowNumbersWidget(15),
-              dividerWidget(),
-              rowNumbersWidget(16),
-              dividerWidget(),
-              rowNumbersWidget(17),
-              dividerWidget(),
-              rowNumbersWidget(18),
-              dividerWidget(),
-              rowNumbersWidget(19),
+          searchFieldWidget(),
+          buttonAddZeroWidget(),
+        ],
+      ),
+    );
+  }
+
+  GestureDetector buttonAddZeroWidget() {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          if (searchController.text.length < 3) {
+            searchController.text = '${searchController.text}0';
+            searchNumber(searchController.text);
+          }
+          _isElevated = true;
+        });
+      },
+      child: AnimatedContainer(
+        duration: const Duration(microseconds: 150),
+        width: 32.0,
+        height: 32.0,
+        margin: const EdgeInsets.only(left: 48, right: 6.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.grey.shade100,
+              Colors.grey.shade100,
             ],
           ),
-        ],
+          boxShadow: !_isElevated
+              ? [
+                  const BoxShadow(
+                    color: Color(0xffccd0d3),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: Offset(4, 4),
+                  ),
+                  const BoxShadow(
+                    color: Colors.white,
+                    spreadRadius: 1,
+                    blurRadius: 5,
+                    offset: Offset(-4, -4),
+                  ),
+                ]
+              : [
+                  const BoxShadow(
+                    color: Color(0xffffffff),
+                    offset: Offset(-4, -4),
+                    blurRadius: 5,
+                    spreadRadius: 1,
+                    inset: true,
+                  ),
+                  const BoxShadow(
+                    color: Color(0xffccd0d3),
+                    offset: Offset(4, 4),
+                    blurRadius: 5,
+                    spreadRadius: 1,
+                    inset: true,
+                  ),
+                ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              '+0',
+              style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w600),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Flexible searchFieldWidget() {
+    return Flexible(
+      child: TextField(
+        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+        controller: searchController,
+        keyboardType: TextInputType.number,
+        maxLength: 3,
+        style: const TextStyle(fontSize: 18.0),
+        decoration: InputDecoration(
+          counter: const Offstage(),
+          prefixIcon: const Icon(Icons.search),
+          suffixIcon: IconButton(
+            onPressed: () {
+              setState(() {
+                searchController.clear();
+                fillArrayNumbers();
+              });
+            },
+            icon: Icon(Icons.close,
+                color: searchController.text.isNotEmpty
+                    ? Colors.black
+                    : Colors.grey[200],
+                size: 20.0),
+          ),
+          hintText: 'Search number',
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(
+              color: Colors.grey.shade600,
+            ),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(
+              color: Color(0xFF3399CC),
+            ),
+          ),
+        ),
+        onChanged: searchNumber,
       ),
     );
   }
@@ -114,12 +200,12 @@ class _LearningScreenState extends State<LearningScreen> {
   GestureDetector rowNumbersWidget(int step) {
     return GestureDetector(
       onTap: () {
-        developer.log(_arrayNumbers[step].toString());
+        developer.log(arrayNumbers[step].toString());
         setState(() {
           _speechButtonOn = true;
           _currentStep = step;
         });
-        speak(globals.sortingMap[_arrayNumbers[step]]);
+        speak(globals.sortingMap[arrayNumbers[step]]);
       },
       child: Padding(
         padding: const EdgeInsets.only(
@@ -136,7 +222,7 @@ class _LearningScreenState extends State<LearningScreen> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Text(
-                        _arrayNumbers[step].toString().padLeft(3, ' '),
+                        arrayNumbers[step].toString().padLeft(3, ' '),
                         style: const TextStyle(fontSize: 18.0),
                         textAlign: TextAlign.center,
                       ),
@@ -146,13 +232,14 @@ class _LearningScreenState extends State<LearningScreen> {
                 Padding(
                   padding: const EdgeInsets.only(left: 10.0),
                   child: Text(
-                    globals.sortingMap[_arrayNumbers[step]],
+                    globals.sortingMap[arrayNumbers[step]],
                     style: const TextStyle(fontSize: 20.0),
                   ),
                 ),
               ],
             ),
             AnimatedContainer(
+              padding: const EdgeInsets.only(right: 10),
               duration: const Duration(milliseconds: 500),
               child: SvgPicture.asset(
                 'assets/images/sound_on.svg',
@@ -168,14 +255,34 @@ class _LearningScreenState extends State<LearningScreen> {
       ),
     );
   }
+
+  void searchNumber(String query) {
+    fillArrayNumbers();
+    final suggestions = arrayNumbers.where((numer) {
+      final numerAsString = numer.toString();
+      return numerAsString.contains(query);
+    }).toList();
+
+    setState(() {
+      arrayNumbers = suggestions;
+    });
+  }
+
+  void fillArrayNumbers() {
+    arrayNumbers.clear();
+    globals.sortingMap.forEach((key, value) {
+      arrayNumbers.add(key);
+    });
+  }
 }
 
 Divider dividerWidget() {
   return const Divider(
-    height: 2,
-    indent: 10.0,
-    endIndent: 10.0,
-    thickness: 2,
+    height: 0,
+    indent: 20.0,
+    endIndent: 20.0,
+    thickness: 1,
+    color: Color(0xFF3399CC),
   );
 }
 
